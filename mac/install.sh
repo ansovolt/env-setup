1c4cb4e59c05f0a3cfec2e5e6c20b8dc22a28798
ansovolt <anso2@hotmail.com>
2019-11-23 12:58:16 -0600

    adding brew

--- mac/install.sh
+++ mac/install.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
 
+#install homebrew
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+
+#brew
+
+#utils
+brew list tmux &>/dev/null || brew install tmux
+brew list curl &>/dev/null || brew install curl
+
+
+#casks
+
+brew cask install iterm2
+brew cask install google-chrome
+brew cask install skype
+brew cask install visual-studio-code
+brew cask install virtualbox
+
+
+
+
